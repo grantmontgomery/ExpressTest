05212b91dcfc1f4b46ab3d39e828401cd751ec63
Grant <gmontgomery1994@gmail.com>
2019-11-15 17:10:45 -1000

    lots of progress made

---
+++ Members.js
@@ -0,0 +1,31 @@
+const members = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "john@gmail.com",
+        status: "active"
+    },
+    {
+        id: 2,
+        name: "Grant Man",
+        email: "grant@gmail.com",
+        status: "active"
+    },
+    {
+        id: 3,
+        name: "Bob Smith",
+        email: "bob@gmail.com",
+        status: "active"
+    },
+    {
+        id: 4,
+        name: "Alex Mann",
+        email: "alex@gmail.com",
+        status: "inactive"
+    },
+    
+
+]
+
+module.exports = members
+
--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express")
 const path = require("path")
+const members = require("./Members")
+
 
 const app = express()
 
-app.get("/", (req, resp) => {
-    resp.sendFile(path.join(__dirname, "public", "index.html"))
+
+
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`);
+    next();
+}
+
+//Init middleware
+app.use(logger)
+
+//Attaches file to a section on local host
+
+// app.get("/", (req, resp) => {
+//     resp.sendFile(path.join(__dirname, "public", "index.html"))
+// })
+
+
+
+//Gets all members
+
+app.get("/api/members", (req, res) => {
+    res.json(members)
 })
+
+//Set static folder
+
+app.use(express.static(path.join(__dirname, "public")))
 
 const PORT = process.env.PORT || 5000
 
